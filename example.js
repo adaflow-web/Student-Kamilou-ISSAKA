f4e192d4fb271718f3dca1fd983f3259a4e49e02
Kamilou 52 <elkemal2000@yahoo.fr>
2023-01-11 16:34:54 +0100

    Ajout de example.js

---
+++ example.js
@@ -0,0 +1,625 @@
+// // console.log("Hello");
+// // console.log("Kamilou")
+// // console.log("HI")
+// // var studentsNumber = 7;
+// // var studentSgender = "femalesmale";
+// // var computermodels = "hpmacos";
+// // console.log(studentsNumber);
+
+// var car1 = "blue ";
+// var car2 = "red ";
+// var car3 = "yellow ";
+// console.log(car1 + car2 + car3);
+
+// // var fruits = 74;
+// // console.log(fruits)
+
+// var bananas = 101;
+// var pears = 78;
+
+// console.log(bananas);
+// console.log(pears);
+
+// var numberOfCoffee = 1;
+// var favoriteFruit = "banana";
+// var myBestSeason = "winter";
+
+// console.log(numberOfCoffee);
+// console.log(favoriteFruit);
+// console.log(myBestSeason);
+
+// var nom = "Khalyda";
+// var greetings = "Hello";
+// message = greetings + nom;
+// console.log(message);
+
+// var nom = "Khalyda";
+// var greetings = "Hello";
+// var space = " "
+// message = greetings + space + nom;
+// console.log(message);
+
+// var nom = " Khalyda";
+// var greetings = "Hello";
+// message = greetings + nom;
+// console.log(message);
+
+// var celsius = 12;
+// var farenheit = (celsius * 9/5 + 32);
+// console.log(farenheit);
+
+// function greet() {
+//  console.log("Hello");   
+// }
+
+// greet();
+
+
+// // // A function that says hello to someone
+// function sayHello(nom)
+// {
+//     console.log("Hello " + nom);
+//   }
+
+//   sayHello("Ahmed");
+
+// //   # To concatenate a value in JS
+
+// function sayHello(nom)
+// {
+//     var result = "Hello " + nom;
+//     return result;
+//   }
+
+//   sayHello();
+
+// //   #Assign a value before running our function
+
+// // 
+
+// // #To make it run
+
+// function sayHello(nom)
+// {
+//     return "Hello " + nom;
+// }
+
+// var message = sayHello("Divine");
+// console.log(message);
+
+// // Call the FUNCTION inside the CONSOLE.LOG 
+
+// function sayHello(nom)
+// {
+//     return "Hello " + nom;
+// }
+
+// console.log(sayHello("Divine"));
+
+// // Some other FUNCTIONS
+
+// function add(a, b) {
+//   return a + b;
+// }
+
+// var result = add(32, 5);
+// console.log(result);
+
+// // Any other ways to do it
+
+// function add(a, b) {
+//     return a + b;
+// }
+
+// console.log(add(7.12, 70));
+
+// function subtract(a, b) {
+//     return a - b;
+// }
+
+// var result = subtract(100, 30);
+// console.log(result);
+
+// function multiply(a, b) {
+//     return a * b;
+// }
+
+// console.log(multiply(0.10, 70));
+
+// // To resume
+
+// function add(a, b) {
+//     return a + b;
+// }
+
+// function subtract(a, b) {
+//     return a - b;
+// }
+
+// function multiply(a, b, c) {
+//     return a * b * c;
+// }
+    
+// console.log(add(34, 11));
+// console.log(subtract(29, 47));
+// console.log(multiply(9, 5, 13));
+
+// // Exercise: Convert my TEMP CONVERTER
+
+// function convert(celsius) {
+//     var farenheit = (celsius * (9/5)) + 32;
+//     console.log(farenheit);
+// }
+
+// convert(11.74);
+// convert(-0.7);
+// convert(11.3);
+// convert(0);
+
+// // console.log(convert(11.74));
+// // console.log(convert(-0.7));
+// // console.log(convert(11.3));
+// // console.log(convert(0));
+    
+
+
+
+    
+
+
+
+
+
+
+
+// //  var sayHello = ("Hello " );
+// //   var nom = "Ahmed";
+// //   result = (sayHello + nom);
+
+// // CONVERT TYPES IN JAVASCRIPT
+
+// var aNumber = 7;
+// var aString = "Hello!";
+// var result = aNumber + aString
+
+// console.log(result);
+
+// // LIKE A CONCATENATE
+
+// var aNumber = 7;
+// var aString = "7";
+// var result = aNumber + aString;
+
+// console.log(result);
+
+// // POSTFIX FUNCTIONSAS.toString()
+
+// var aNumber = 5;
+// var aString = "Hello";
+// var result = aNumber.toString() + aString;
+
+// console.log(result);
+
+// // THE Number function
+
+// var aNumber = 5;
+// var aString = "3";
+// var result = aNumber + Number(aString);
+
+// console.log(result); 
+
+// // ANOTHER WAY TO CONVERT A VALUE
+
+// function add() {
+//     var text = "The result is ";
+//     var number = "9";
+//     var result = text + Number(number);
+//     return result;
+// }
+
+// console.log(result);
+
+// // OTHER ELSE
+
+// function addTwo(text, number) {
+//     return text + number.toString();
+
+// }
+
+// console.log(addTwo("The result is ", 9));
+
+// // // FRIENDLY CONVERTER
+
+// // function agrairianmeasureConverter(meter)
+// // { var meter = agrairian *100
+// // var result = agrairian * 100;
+// // var res = result.toFixed(7);
+// // var msg1 = "The conversion from ";
+// // var msg2 = " agrairians is ";
+// // return msg1 + agrairian.toSting() + msg2 + res.toString() + msg3;
+// // }
+
+// // console.log(agrairianmeasureConverter(27));
+
+// // CONVERTING TYPES IN JAVASCRIPT
+
+// var aNumber = 5;
+// var aString = "Hello";
+// var result = aNumber + aString;
+
+// console.log(result);
+
+// // CONCATENATE TWO STRINGS IN JAVASCRIPT
+
+// var aNumber = 5;
+// var aString = "3";
+// var result = aNumber + aString;
+
+// console.log(result);
+
+// // THE USE OF toString() FUNCTION
+
+// var aNumber = 5;
+// var aString = "Hello";
+// var result = aNumber.toString() + aString;
+
+// console.log(result);
+
+// // THE Number Function()
+
+// var aNumber = 9;
+// var aString = "1";
+// var result = aNumber + Number(aString);
+
+// console.log(result);
+
+
+// // USING ADD FUNCTION TO CONCATENATE TWO STRINGS : THIS WILL RUN AN ERROR, THEN WE USE .toString() to convert data type in javasript
+// function add(a, b) {
+//     var message = "The result is ";
+//     var c = a + b;
+//     return message + c
+// }
+// console.log(add(9, 7));
+
+// // CONVERT OUR DATA TYPE IN jS
+
+// function add(a, b) {
+//     var message = "The result is ";
+//     var c = a + b;
+//     return message + c.toString();
+// }
+
+// console.log(add(9, 7));
+
+// // My GnD to SWISS FRANC CONVERTER
+
+// function gnd_converter(gnd, exchange_rate) {
+//     var chf = gnd * exchange_rate; 
+//     var gnd_converter = "Today, GnD " + gnd.toString() + " can buy CHF " + chf.toString() + ".";
+//     return gnd_converter;
+// }
+
+// console.log(gnd_converter(1000,780));
+
+// // Km to inches CONVERTER
+
+// function convert(km) {
+//     return 0.621371/km;
+// }
+// x = 2;
+// console.log("The conversion from " + x.toString() + " km" + " is " + convert(x).toString() + " miles." );
+
+// // The Booleans
+
+// var day = true;
+// var night = false;
+
+// console.log(day);
+// console.log(night);
+
+// // Convert a short program to JS
+
+// var secretNumber = 7;
+// var guess = 13;
+// console.log(secretNumber == Number(guess));
+
+// // IF THIS THEN WHAT
+
+// var secret_word = "Exploration"
+// var guess = prompt("Try to guess my favourite word: ")
+
+// if (secret_word == guess) {
+//     console.log("Congratulations! You've found the secret word!")
+// }
+// if (secret_word != guess) {
+//     console.log("Try again!")
+
+
+// // SAME CONCEPT, DIFFERENT LANGUAGES
+
+// var apples = 7;
+// var oranges = 3;
+
+// if (apples >= oranges) {
+//   console.log("There are more apples than oranges...");
+//   console.log("...or the same amount!");
+// }
+
+// console.log("The program terminated successfully");
+
+// // ANOTHER EXAMPLE
+
+// var apples = 7;
+// var oranges = 3;
+
+// if (apples <= oranges) {
+//   console.log("There are less apples than oranges...");
+//   console.log("...or the same amount!");
+// }
+
+// console.log("The program terminated successfully");
+
+// // CONCATENATE
+// var a = prompt("Please enter the first number X for addition: ");
+// var b = prompt("Please enter the second number Y for addition: ");
+// var c = prompt("Try go guess, what is X + Y?: ");
+// var d = Number(a) + Number(b);
+
+// if (Number(c)==Number(d)) {
+//     console.log("Congratulations! You found the correct result.");
+// }
+// if (Number(c)!=Number(d)){
+//     console.log("Try again! The answer is close, you've got this.");
+// }
+// console.log("The program terminated successfully.");
+
+// // CONCATENATE WITH .toString
+
+// // var a = prompt("Please enter the first number X for addition: ");
+// // var b = prompt("Please enter the second number Y for addition: ");
+// // var c = prompt("Try go guess, what is X + Y?: ");
+// // var d = a + b;
+
+// // if (c.toString == d.toString) {
+// //     console.log("Congratulations! You found the correct result.");
+// // }
+// // if (c.toString != d.toString) {
+// //     console.log("Try again! The answer is close, you've got this.");
+// // }
+// // console.log("The program terminated successfully.");
+
+// // THE SECRET WORD UPDATED
+
+// // var secret_word = "Exploration"
+// // var guess = prompt("Try to guess my favourite word: ")
+
+// // if (secret_word == guess) {
+// //     console.log("Congratulations! You've found successfully the secret word!")
+// // }
+// // if (secret_word != guess) {
+// //     console.log("Try again!")
+// // }
+
+// // APPLY IF this, THEN.. TO MY CONVERTER
+
+// var c = prompt("How many degrees Celsius would you like to convert? ");  
+
+// function convTemp(c) {  
+//     var msg1 = " degrees Celsius are ";
+//     var msg2 = " degrees Fahrenheit.";
+//     f = question * 9/5 + 32;
+//     return Number(c) + msg1 + Number(f) + msg2;
+//   }
+
+// if (c < Number(0)) {
+//     console.log("Man it's cold out there!");
+//   }
+// if (c > Number(0)) {
+//     console.log("Above 0 degrees Celsius, you can swim in the lake.");
+// }
+
+// ELSE , JAVASCRIPT EDITION
+
+// var apples = 3;
+// var oranges = 11;
+
+// if (apples >= oranges) {
+//   console.log("There are more apples than oranges...");
+//   console.log("...or the same amount!");
+// } else {
+//   console.log("There are more oranges!");
+// }
+
+// console.log("The program executed properly.")
+
+// SOLUTION OF ADDING TWO NUMBERS
+
+// var number_1 = 7;
+// var number_2 = 4;
+// var result = number_1 + number_2;
+
+// if (result > 10) {
+//     console.log("The result is grearer than 10");
+// } else {
+//     console.log("The result is not graeter than 10")
+// }
+
+// console.log("Try me again!");
+
+// For loop in Javascript
+
+// var i = 0;
+// while (i < 5) {
+//   console.log("Hello " + i.toString());
+//   i = i + 1;
+// }
+
+// TEST FOR LOOP
+
+// function test_for() {
+//     for(var i = 1; i < 5; i = i+1){
+//     if (i == 1) {
+//     console.log(i.toString() + " step");
+//     }
+//     else if (i < 4 && i != 1) {
+//     console.log(i.toString() + " steps");
+//     }
+//     else {
+//     console.log("Done!");
+//     }
+//     }
+//     }
+    
+//     test_for();
+
+// TEST WHILE LOOP
+
+// var a = 1;
+// while (a < 6) {
+//     var step = "";
+//     if (a == 1) {
+//         step = " step";
+//     } else {
+//         step = " steps";
+//     }
+//     console.log(a.toString() + step);
+//     a = a + 1;
+// } 
+
+// SOME WHILE LOOP
+
+// var a = 1;
+// while (a < 6) {
+//     var step = "";
+//     if (a == 1) {
+//         step = " step";
+//     } else {
+//         step = " steps";
+//     }
+//     console.log(a.toString() + step);
+//     a = a + 1;
+// } 
+// console.log("Done!");
+
+// LOOPING ARRAW IN JAVASCRIPT
+
+// var fruits = ["apples", "oranges", "bananas"];
+//  for(i = 0; i < fruits.length; i = i + 1) {
+//    console.log(fruits[i]);
+//  }
+
+// LOOPING THROUGH MY FAVORITE FOOD
+
+// var best_foods = ["Watché", 
+//             "Coliko", 
+//             "Akoumé", 
+//             "Touwoo","Abloo",];
+
+// console.log("Here are my favorite foods!");
+
+// for (i = 0; i < best_foods.length; i = i + 1) {
+//     console.log(best_foods[i]);
+// }
+
+// console.log("what is your best_foods?");
+
+// ANOTHER VIEW OF OUR LOOP
+
+// var best_foods = ["Watché", 
+//             "Coliko", 
+//             "Akoumé", 
+//             "Touwoo","Abloo",];
+
+// console.log("Here are my favorite foods!");
+
+// for (i = 0; i < best_foods.length; i = i + 1) {
+//     console.log(best_foods[i]);
+//     console.log("what is your best_foods?");
+// }
+
+// TO PRINT MY MOST PREFERED FOOD MY CAN BE 
+
+// var best_foods = ["Watché", 
+//             "Coliko", 
+//             "Akoumé", 
+//             "Touwoo","Abloo",];
+
+// console.log("My most favorite food is!");
+
+// for (i = 0; i < best_foods.length; i = i + 1) {
+//     console.log(best_foods[2]);
+// }
+
+// console.log("what is yours?");
+
+// OBJECTS IN JS
+
+// var city = {name: "Leysin", country: "Switzerland", elevation: 1263}
+
+// console.log(city.country);
+// console.log(city["name"]);
+
+// POSSIBLE TO PRINT COUNTRY BEFORE CITY NAME
+
+// var city = {name: "Leysin", country: "Switzerland", elevation: 1263}
+
+// console.log(city["name"]);
+// console.log(city.country);
+
+// ALSO SOME WAYS TO PRINT OTHERS
+
+// var city = {name: "Leysin", country: "Switzerland", elevation: 1263}
+
+// console.log(city.name);
+// console.log(city["country"]);
+
+// var city = {name: "Leysin", country: "Switzerland", elevation: 1263}
+
+// console.log(city.name, city.elevation);
+// console.log(city["country"]);
+
+// ANOTHER ONE
+
+// var city = {name: "Leysin", country: "Switzerland", elevation: 1263}
+
+// console.log(city.name);
+// console.log(city["elevation"]);
+
+// ANOTHER AGAIN
+
+// var city = {name: "Leysin,", country: "Switzerland.", elevation: 1263}
+
+// console.log(city.name, city.elevation, city.country);
+// console.log("Your're Welcome in our Ski earier!", "Wilkommen hier!");
+
+// CONVERT MY BUSINESS CARD PROGRAMMER TO JAVASCRIPT
+
+// var myBusiness_card = {"Name": "El",
+//                     "Last name": "Kémal",
+//                     "City": "SwissVaud", 
+//                     "Country": "Switzerland",
+//                     "Cell Phone":  9973111197,
+//                    "Email": "wwwkemal2023@swisscom.ch"}
+
+//  console.log(myBusiness_card.Name, myBusiness_card.toString("Last name"), myBusiness_card.toString("Cell Phone"), myBusiness_card.City, myBusiness_card.Country, myBusiness_card.toString("Email"));
+//  console.log(myBusiness_card["City"]);
+
+// CONVERT MY BUSINESS CARD PROGRAMMER TO JAVASCRIPT
+
+// var myBusiness_card = {"Name": "El",
+//                     "Last name": "Kémal",
+//                     "City": "SwissVaud", 
+//                     "Country": "Switzerland",
+//                     "Cell Phone":  9973111197,
+//                    "Email": "wwwkemal2023@swisscom.ch"};
+
+// console.log(myBusiness_card.Name, myBusiness_card["Last name"], myBusiness_card.City, myBusiness_card.Country, myBusiness_card["Cell Phone"], myBusiness_card.Email);
+
+// RANDOM NUMBERS IN JAVASCRIPT
+
+
+var a = Math.random() * 101;
+a = Math.floor(a) + 1;
+
+console.log(a);
