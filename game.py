ccd3d1119f4d140e9f2ee0b09a2654de9feb1891
Kamilou 52 <elkemal2000@yahoo.fr>
2023-03-13 12:46:10 +0100

    Added the initial setup

---
+++ game.py
@@ -0,0 +1,17 @@
+# Setup
+
+#Game loop
+
+        #Get user input
+        
+        
+        # Check the user input
+        
+        #Increase attempt count
+        
+        #Check  the user answer against the secret number
+        
+#Get the spelling of the "attempt" word 
+
+# Display the result or congratulation message   
+        
