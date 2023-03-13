8a2d34c0d4125089ee2ae1e767bbb33a319c46ae
Kamilou 52 <elkemal2000@yahoo.fr>
2023-03-13 13:24:24 +0100

    Get user input done

--- game.py
+++ game.py
@@ -5,12 +5,18 @@ user_wins = False
 attempts = 0
 
 #Game loop
+while user_wins != True:
 
         #Get user input
+        guess =input("Enter a number between 1 and 100:")
         
         
         # Check the user input
-        
+        try:
+            guess_number = int(guess)
+        except:
+            print("Error: Please a valid number is needed!")
+            quit()
         #Increase attempt count
         
         #Check  the user answer against the secret number
